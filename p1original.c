92d0f83f9dc73d360fc2404fdcb5485c689fb6f9
saurabh16107372 <95605060+saurabh1610737281@users.noreply.github.com>
2022-02-04 05:13:53 +0000

    coding

--- p1original.c
+++ p1original.c
@@ -1,2 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+
+	int num1, num2, result;
+
+	printf("Enter the two number: \n");
+	scanf("%d %d", &num1, &num2);
+
+	result=(num1,num2);
+
+	printf("\nAddition of %d and %d is %d\n", num1, num2, result);
+
+}
+
+int add(int *no1, int *no2)
+{
+	int res;
+	res = *no1 + *no2;
+	return res;
 }
